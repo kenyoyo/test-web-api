d88cbe5f8e28c707df66da6e168fd4d781f6c0b8
toeytom <toeytom2551@hotmail.com>
2017-11-23 18:28:29 +0700

    kuy

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 const express =require('express')
 const app = express()
 app.get('/',(req, res)=>res.send('HelloWorld'))
-app.listen(5000,()=>console.log('Hello'))
+app.listen(5000,()=>console.log('Helloworld'))
