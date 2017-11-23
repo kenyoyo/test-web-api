e37720e9bf659f5d0f97f9ba9be40099e4eb436e
toeytom <toeytom2551@hotmail.com>
2017-11-23 22:26:20 +0700

    fix server

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express =require('express')
 const app = express()
 app.get('/',(req, res)=>res.send('HelloWorld'))
-<<<<<<< HEAD
 app.listen(5000,()=>console.log('Hello'))
-=======
->>>>>>> d88cbe5f8e28c707df66da6e168fd4d781f6c0b8
+
